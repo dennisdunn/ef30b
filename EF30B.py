31f950dab736e75762a4568a7301dc51d495407a
Dennis Dunn <ansofive@gmail.com>
2023-11-24 18:00:19 -0500

    Stuff.

--- EF30B.py
+++ EF30B.py
@@ -1,44 +1,36 @@
 from machine import Pin, Timer
+import time
 
-class DigitSelect:
-    def __init__(self, gpio) -> None:
-        self.__pin = Pin(gpio, Pin.OPEN_DRAIN, value=1)
-        
+class LedGroup:
+    def __init__(self, groupGpio, *leds):
+        self.__pin = Pin(groupGpio, Pin.OPEN_DRAIN, value=1)
+        self.__leds = leds
+        self.__state = None
+
     def enabled(self, enabled=None):
         if enabled == None:
             return not self.__pin.value()
         else:
             self.__pin.value(not enabled)
-            
-    def enable(self):
-        self.enabled(True)
-            
-    def disable(self):
-        self.enabled(False)
-        
-    
-class Segment:
-    def __init__(self, gpio) -> None:
-        self.__pin = Pin(gpio, Pin.OUT)
-            
-    def value(self, value=None):
-        return self.__pin.value(value)
-        
+
+    def state(self, values=None):
+        self.__state = values
+        if values != None:
+            for led, value in zip(self.__leds, values):
+                led.value(value)
+
     def on(self):
-        self.value(True)
-        
-    def off(self):
-        self.value(False)
-        
-    def toggle(self):
-        if self.value():
-            self.off()
+        if self.__state != None:
+            self.enabled(True)
         else:
-            self.on()
-            
-class SegmentController:
-    def __init__(self, *segments) -> None:
-        self.__segments = segments
+            self.enabled(False)
+
+    def off(self):
+        self.enabled(False)
+
+class SevenSegment(LedGroup):
+    def __init__(self, groupGpio, *leds):
+        super().__init__(groupGpio, *leds)
         self.__lookup = [[1,1,1,1,1,1,0],
                          [0,1,1,0,0,0,0],
                          [1,1,0,1,1,0,1],
@@ -49,54 +41,42 @@ class SegmentController:
                          [1,1,1,0,0,0,0],
                          [1,1,1,1,1,1,1],
                          [1,1,1,0,0,1,1]]
-
-    @staticmethod
-    def create(*gpios):
-        return SegmentController([Segment(gpio) for gpio in gpios])
-
-    def update(self, n):
-        for i in range(len(self.__segments)):
-            self.__segments[i].value(self.__lookup[n][i])         
-
-class MultipleDigitDisplay:
-    def __init__(self, segmentController, *digitSelectors):
-        self.__controller = segmentController
-        self.__selectors = digitSelectors
-        self.__values = [-1]*len(digitSelectors)
-        self.__timer = Timer()
-
-    @staticmethod
-    def create(segmentController, *gpios):
-        return MultipleDigitDisplay(segmentController,[DigitSelector(gpio) for gpio in gpios])
-            
-    def value(self, value):
-        values = [-1]*len(self.__selectors) + [int(d) for d in str(value)]
-        values.reverse()
-        end=len(self.__selectors)
-        values = values[0:end] 
-        values.reverse()
-        self.__values = values
-            
-    def on(self):
-        
-        def update(timer):
-            for display,value in zip(self.__selectors,self.__values):
-                self.__controller.update(value)
-                display.enable()
-                time.sleep(1/100)
-                display.disable()
-                
-        for display in self.__selectors:
-            display.enabled(True)
-            
-        self.__timer.init(mode=Timer.PERIODIC, freq=100, callback=update)
     
-    def off(self):
-        self.__timer.deinit()
-        for display in self.__selectors:
-            display.enabled(False)
-        
-    def clear(self):
-        self.off()
-               
+    def value(self, n=None):
+        if n == None:
+            super().state(None)
+        else:
+            super().state(self.__lookup[n])
 
+class LedGroupMultiplexer:
+    def __init__(self, *groups):
+        self.__groups = groups
+        self.__running = False
+        self.__timer = Timer
+        self.freq = 30
+
+    def __tick(self):
+        for group in groups:
+            group.on()
+            time.sleep(len(self.__groups)/self.freq)
+            group.off()
+
+    def value(self, n):
+        for group, m in zip(self.__groups,[int(d) for d in str(n)]):
+            group.value(m)
+
+    def start(self):
+        self.__timer.init(mode=Timer.PERIODIC, freq=self.freq)
+        self.__running = True
+
+    def stop(self):
+        self.__timer.deinit()
+        self.__running = False
+
+               
+# test env
+
+leds = [Pin(n, Pin.OUT) for n in [13,14,15,16,17,18,19]]
+msb = SevenSegment(2, *leds)
+lsb = SevenSegment(3, *leds)
+plex = LedGroupMultiplexer(msb,lsb)
--- board.py
+++ board.py
@@ -6,6 +6,7 @@ DS3 = EF30B.DigitSelect(4)
 
 segments = EF30B.SegmentController(EF30B.Segment(13),EF30B.Segment(14),EF30B.Segment(15),EF30B.Segment(16),SEF30B.egment(17),EF30B.Segment(18),EF30B.Segment(19))
 
+leds = [Led(n) for n in [13,14,15,16,17,18,19]]
 disp = EF30B.MultipleDigitDisplay(segments, DS1, DS2)
 
 disp.on()
